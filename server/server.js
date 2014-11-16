c8f52fad90b6dd18936a97a03517d935f18d81ad
Jan Jongboom <janjongboom@gmail.com>
2014-11-16 21:50:44 +0600

    Add devicemotion demo

--- server/server.js
+++ server/server.js
@@ -4,20 +4,22 @@ connect()
     .use(connect.static(__dirname))
     .listen(8321, '0.0.0.0');
 
-var WebSocketServer = require('ws').Server,
-    wss = new WebSocketServer({ port: 8322, host: '0.0.0.0' });
-
-wss.broadcast = function(data) {
-  for (var i in this.clients)
-    this.clients[i].send(data);
-};
-
-wss.on('connection', function(ws) {
-  console.log('new connection');
-  ws.on('message', function(message) {
-    console.log('got message', message);
-    wss.broadcast(message);
+var ws = require("nodejs-websocket");
+var server = ws.createServer(function(conn) {
+  console.log("New connection");
+  conn.on("text", function(str) {
+    console.log("Received " + str);
+    broadcast(str);
   });
-});
+  conn.on("close", function(code, reason) {
+    console.log("Connection closed");
+  });
+}).listen(8322, '0.0.0.0');
+
+function broadcast(msg) {
+  server.connections.forEach(function(conn) {
+    conn.sendText(msg);
+  });
+}
 
 console.log('Listening on port', 8321, 8322);
